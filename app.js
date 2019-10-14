abe59888f05211316533232abe721ee09de98583
Oriana Dentici <oriana.c.dentici@gmail.com>
2019-10-13 22:27:56 -0500

    code quiz -- will be continuing to work on; ignore playpen.html

---
+++ app.js
@@ -0,0 +1,35 @@
+/* global vars*/
+var questionDiv = document.getElementById("question");
+var choicesDiv = document.getElementById("choices");
+var questionBtn = document.getElementById("questionBtn");
+var startQuestion = 0;
+//var answers = 0;
+
+/* helpers */
+/* takes in index, uses that index to find question in db to return the title */
+function renderQuestion(index) {
+  return questions[index].title;
+}
+/* takes in index, uses this index to return the choices from the db */
+function renderChoices(index) {
+  choicesDiv.innerHTML = "";
+  for (i = 0; i < questions[index].choices.length; i++) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = questions[index].choices[i];
+    choicesDiv.appendChild(choiceBtn);
+  }
+}
+
+/* events */
+questionBtn.addEventListener("click", function() {
+  startQuestion++;
+  /* call redner question */
+  questionDiv.innerHTML = renderQuestion(startQuestion);
+  renderChoices(startQuestion);
+});
+
+/* init */
+questionDiv.innerHTML = renderQuestion(startQuestion);
+renderChoices(0);
+
+/*local storage to gather responses*/
---
+++ questions.js
@@ -0,0 +1,80 @@
+var questions = [
+  {
+    title: "Which of the following is a kind of lighting fixture?",
+    choices: ["Fresnel", "Flamel", "Flannel", "Freshener"],
+    answer: "A"
+  },
+
+  {
+    title: "What is a 'lamp'?",
+    choices: [
+      "Set dressing",
+      "The glass bulb inside a light",
+      "An outdated acronym regarding sandwiches",
+      "The home of a genie"
+    ],
+    answer: "B"
+  },
+
+  {
+    title:
+      "What basic colors of LED's do you want to mix to make an amber light?",
+    choices: [
+      "Orange and White",
+      "Blue and Green",
+      "Red and Green",
+      "UV and Orange"
+    ],
+    answer: "C"
+  },
+
+  {
+    title: "When sitting in the audience stage left is where?",
+    choices: [
+      "To your right",
+      "To your left",
+      "Behind the curtains",
+      "The stage has left the building"
+    ],
+    answer: "A"
+  },
+
+  {
+    title: "They call it downstage because:",
+    choices: [
+      "Stage direction terms were based off raked stages",
+      "Look down, there it is",
+      "Direction in a 3D universie is relative",
+      "It's where the sad actors go"
+    ],
+    answer: "A"
+  },
+
+  {
+    title: "Where are you least likely to find an actor:",
+    choices: [
+      "Dressing room",
+      "Bathroom",
+      "Their light",
+      "Touching props that aren't theirs"
+    ],
+    answer: "C"
+  },
+
+  {
+    title: "Which of these are sound cables:",
+    choices: ["XRT", "XTC", "XLR", "XCL"],
+    correct: "B"
+  },
+
+  {
+    title: "Why isn't the sound working?",
+    choices: [
+      "The board is off",
+      "The cables are not properly plugged in",
+      "God has abandoned us",
+      "A and B"
+    ],
+    answer: "D"
+  }
+];
