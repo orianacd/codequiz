69d6216a15d7cd8ec7ba91c62b80ed8465559b15
Oriana Dentici <oriana.c.dentici@gmail.com>
2020-03-29 18:40:10 -0500

    updates

--- questions.js
+++ questions.js
@@ -1,80 +1,83 @@
 var questions = [
   {
-    title: "Which of the following is a kind of lighting fixture?",
-    choices: ["Fresnel", "Flamel", "Flannel", "Freshener"],
-    answer: "A"
+    question: "Which of the following is a kind of lighting fixture?",
+    choiceA: "Fresnel",
+    choiceB: "Flamel",
+    choiceC: "Flannel",
+    choiceD: "Freshener",
+    correct: "A"
   },
 
   {
-    title: "What is a 'lamp'?",
-    choices: [
-      "Set dressing",
-      "The glass bulb inside a light",
-      "An outdated acronym regarding sandwiches",
-      "The home of a genie"
-    ],
-    answer: "B"
+    question: "What is a 'lamp'?",
+    : [
+  "Set dressing",
+  "The glass bulb inside a light",
+  "An outdated acronym regarding sandwiches",
+  "The home of a genie"
+],
+  correct: "B"
   },
 
-  {
-    title:
-      "What basic colors of LED's do you want to mix to make an amber light?",
-    choices: [
-      "Orange and White",
-      "Blue and Green",
-      "Red and Green",
-      "UV and Orange"
-    ],
-    answer: "C"
-  },
+{
+  question:
+  "What basic colors of LED's do you want to mix to make an amber light?",
+    : [
+    "Orange and White",
+    "Blue and Green",
+    "Red and Green",
+    "UV and Orange"
+  ],
+    correct: "C"
+},
 
-  {
-    title: "When sitting in the audience stage left is where?",
-    choices: [
-      "To your right",
-      "To your left",
-      "Behind the curtains",
-      "The stage has left the building"
-    ],
-    answer: "A"
-  },
+{
+  question: "When sitting in the audience stage left is where?",
+    : [
+    "To your right",
+    "To your left",
+    "Behind the curtains",
+    "The stage has left the building"
+  ],
+    correct: "A"
+},
 
-  {
-    title: "They call it downstage because:",
-    choices: [
-      "Stage direction terms were based off raked stages",
-      "Look down, there it is",
-      "Direction in a 3D universie is relative",
-      "It's where the sad actors go"
-    ],
-    answer: "A"
-  },
+{
+  question: "They call it downstage because:",
+    : [
+    "Stage direction terms were based off raked stages",
+    "Look down, there it is",
+    "Direction in a 3D universie is relative",
+    "It's where the sad actors go"
+  ],
+    correct: "A"
+},
 
-  {
-    title: "Where are you least likely to find an actor:",
-    choices: [
-      "Dressing room",
-      "Bathroom",
-      "Their light",
-      "Touching props that aren't theirs"
-    ],
-    answer: "C"
-  },
+{
+  question: "Where are you least likely to find an actor:",
+    : [
+    "Dressing room",
+    "Bathroom",
+    "Their light",
+    "Touching props that aren't theirs"
+  ],
+    correct: "C"
+},
 
-  {
-    title: "Which of these are sound cables:",
-    choices: ["XRT", "XTC", "XLR", "XCL"],
+{
+  question: "Which of these are sound cables:",
+    : ["XRT", "XTC", "XLR", "XCL"],
     correct: "B"
-  },
+},
 
-  {
-    title: "Why isn't the sound working?",
-    choices: [
-      "The board is off",
-      "The cables are not properly plugged in",
-      "God has abandoned us",
-      "A and B"
-    ],
-    answer: "D"
-  }
+{
+  title: "Why isn't the sound working?",
+    : [
+    "The board is off",
+    "The cables are not properly plugged in",
+    "God has abandoned us",
+    "A and B"
+  ],
+    correct: "D"
+}
 ];
